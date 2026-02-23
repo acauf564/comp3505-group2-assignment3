22560adabcb2c7d5370319058184b17a9baea6d6
nsriv531 <nsriv531@mtroyal.ca>
2026-02-22 23:17:54 -0700

    changed

--- test/org/jfree/data/EqualsTest.java
+++ test/org/jfree/data/EqualsTest.java
@@ -26,17 +26,12 @@ class EqualsTest {
         assertFalse(r1.equals(r2));
     }
 
-    @Test
+   @Test
     void testDifferentUpperReturnsFalseForEquals() {
-        Range r1 = new Range(1.0, 5.0);
-        Range r2 = new Range(1.0, 6.0);
-
-        assertEquals(1.0, r1.getLowerBound());
-        assertEquals(1.0, r2.getLowerBound());
-        assertNotEquals(r1.getUpperBound(), r2.getUpperBound());
-
-        assertNotEquals(r1, r2); // same meaning as assertFalse(r1.equals(r2))
-    }
+    Range r1 = new Range(1.0, 5.0);
+    Range r2 = new Range(1.0, 6.0);
+    assertFalse(r1.equals(r2));
+}
 
     @Test
     void testNullReturnsFalseForEquals() {
