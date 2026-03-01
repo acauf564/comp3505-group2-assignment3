848a1bd88a57ad1f216444ff0e086bbc2ac0a8cc
acauf564 <acauf564@mtroyal.ca>
2026-02-28 21:04:17 -0700

    Renamed tests to reflect correct exception type

--- test/org/jfree/data/DataUtilitiesNullInputTest.java
+++ test/org/jfree/data/DataUtilitiesNullInputTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 public class DataUtilitiesNullInputTest {
 
     @Test
-    public void testCalculateColumnTotalNullDataThrowsNullPointerException() {
+    public void testCalculateColumnTotalNullDataThrowsInvalidParameter() {
         assertThrows(InvalidParameterException.class, () -> {
             DataUtilities.calculateColumnTotal(null, 0);
         });
     }
 
     @Test
-    public void testCalculateRowTotalNullDataThrowsNullPointerException() {
+    public void testCalculateRowTotalNullDataThrowsInvalidParameter() {
         assertThrows(InvalidParameterException.class, () -> {
             DataUtilities.calculateRowTotal(null, 0);
         });
