b1486194c6af01bed615a9655f6335757852e9a2
nsriv531 <nsriv531@mtroyal.ca>
2026-02-28 20:27:56 -0700

    changed the exception handling

--- test/org/jfree/data/DataUtilitiesNullInputTest.java
+++ test/org/jfree/data/DataUtilitiesNullInputTest.java
@@ -1,20 +1,21 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.security.InvalidParameterException;
 import org.junit.jupiter.api.Test;
 
 public class DataUtilitiesNullInputTest {
 
     @Test
     public void testCalculateColumnTotalNullDataThrowsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> {
+        assertThrows(InvalidParameterException.class, () -> {
             DataUtilities.calculateColumnTotal(null, 0);
         });
     }
 
     @Test
     public void testCalculateRowTotalNullDataThrowsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> {
+        assertThrows(InvalidParameterException.class, () -> {
             DataUtilities.calculateRowTotal(null, 0);
         });
     }
